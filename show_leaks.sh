8a5719c2d29ffbbfdb1a588a505b1498480f9427
Angelos-Dimoglis <angelos.titos@gmail.com>
2025-03-08 19:33:35 +0200

    deleted some mac files and modified leaks script

--- show_leaks.sh
+++ show_leaks.sh
@@ -2,12 +2,6 @@
 
 # "test_${i}.out" add this as a second parameter to lex.out to review output
 
-i=1
 for test_file in tests/phase1/*; do
-    valgrind --leak-check=full ./lex.out "$test_file"  > "valgrind_${i}.out" 2>&1
-    ((i++))
+    valgrind --leak-check=full ./lex.out "$test_file" 2>&1 | grep "definitely lost"
 done
-
-grep "definitely lost" valgrind_*.out
-
-rm -v valgrind_*.out
