0d3155be0d7118e9f56176dfb362620e5610448c
ArPapar <apatramanis@yahoo.com>
2025-03-31 20:26:46 +0300

    This has not been tested

--- sym_table.cpp
+++ sym_table.cpp
@@ -1,5 +1,20 @@
 #include "sym_table.h"
 #include <iostream>
+#include <memory>
+
+node* SymTable::scopeNode(unsigned int scope) {
+    node* current;
+    for (int i = 0; i < tableSize; i++) {
+        current = table[i];
+        while (current != nullptr) {
+            if (current->sym.scope = scope) {
+                return current;
+            }
+            current = current->nextCollision;
+        }
+    }
+    return nullptr;
+}
 
 int SymTable::hashFunction(const std::string& key) {
     int hash = 0;
@@ -9,25 +24,66 @@ int SymTable::hashFunction(const std::string& key) {
     return hash % tableSize;
 }
 
-void SymTable::Insert(const std::string& name, unsigned int scope, bool isActive,
+Symbol* createSymbol(enum SymbolType type) {
+    if (type < 4) {
+        return new Variable();
+    }
+    else {
+        return new Function();
+    }
+}
+
+void SymTable::Insert(const std::string& name, unsigned int scope,
                         enum SymbolType type, std::list<Variable> arguments) {
-    
-    if (Lookup(name).name == NULL) {
+    if (Lookup(name).name.empty()) {
         int index = hashFunction(name);
-        Symbol* newSymbol = new Symbol();
+        Symbol* newSymbol = createSymbol(type);
+        node* currentCollision = table[index];
+        if (currentCollision != nullptr) {
+            while (currentCollision->nextCollision) {
+                currentCollision = currentCollision->nextCollision;
+            }
+        }
+        node* currentScope = scopeNode(scope);
+        if (currentScope != nullptr) {
+            while (currentScope->nextScope != nullptr) {
+                currentScope = currentScope->nextScope;
+            }
+        }
         newSymbol->name = name;
-        newSymbol
+        newSymbol->scope = scope;
+        newSymbol->type = type;
+        newSymbol->isActive = true;
+        if (type > 3) {
+            ((Function*)newSymbol)->arguments = arguments;
+        }
+        node* n = new node(*newSymbol);
+        if (currentCollision != nullptr) {
+            currentCollision->nextCollision = n;
+        }
+        else {
+            table[index] = n;
+        }
+        if (currentScope != nullptr) {
+            currentScope->nextScope = n;
+        }
     }
 }
 
 Symbol SymTable::Lookup(const std::string& name) {
     int index = hashFunction(name);
-    for (const auto& node : table[index]) {
-        if (node.name == name) {
-            return node;
+    node* current = table[index];
+    while (current != nullptr) {
+        if (current->sym.name == name) {
+            return current->sym;
         }
+        current = current->nextCollision;
     }
     Symbol* temp = new Symbol();
-    temp->name = NULL;
     return *temp;
+}
+
+void SymTable::Hide(const std::string& name) {
+    Symbol hSym = Lookup(name);
+    hSym.isActive = false;
 }
--- sym_table.h
+++ sym_table.h
@@ -15,30 +15,39 @@ class Symbol {
         unsigned int scope;
         bool isActive;     
         enum SymbolType type;
-        Symbol* scopeNext;
 };
 
-class Variable : Symbol {};
+class Variable : public Symbol {};
 
-class Function : Symbol {
+class Function : public Symbol {
     public:
         std::list<Variable> arguments;
+};
+
+struct node {
+    Symbol sym;
+    node* nextCollision;
+    node* nextScope;
+
+    node(Symbol s) : sym(s), nextCollision(nullptr), nextScope(nullptr) {}
 };
 
 class SymTable {
     private:
         static const int tableSize = 1987;
-        std::list<Symbol> table[tableSize];
+        node* table[tableSize] = {nullptr};
         int hashFunction(const std::string& key);
 
     public:
-        void Insert(const std::string& name, unsigned int scope, bool isActive,
+        void Insert(const std::string& name, unsigned int scope,
                     enum SymbolType type, std::list<Variable> arguments);
 
         Symbol Lookup(const std::string& name);
 
         void Hide(const std::string& name);
 
+        node* scopeNode(unsigned int scope);
+        
         void PrintTable();
 };
 
