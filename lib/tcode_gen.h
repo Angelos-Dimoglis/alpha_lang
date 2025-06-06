c67706e5bd8c7c0c76864201b5ff56f8c613c601
Angelos-Dimoglis <angelos.titos@gmail.com>
2025-06-06 15:15:29 +0300

    added instruction struct

--- lib/tcode_gen.h
+++ lib/tcode_gen.h
@@ -1,5 +1,13 @@
-
 struct vmarg {
     vmarg_t type;
     unsigned val;
 };
+
+struct instruction {
+    vmopcode opcode;
+    vmarg result;
+    vmarg arg1;
+    vmarg arg2;
+    unsigned srcLine;
+};
+
