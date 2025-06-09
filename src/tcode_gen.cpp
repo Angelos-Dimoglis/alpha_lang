04e055288f57ed2afc8e20c2d613895a54c38bee
Angelos-Dimoglis <angelos.titos@gmail.com>
2025-06-09 12:17:39 +0300

    moved avm_table methods to vm

--- src/tcode_gen.cpp
+++ src/tcode_gen.cpp
@@ -1,12 +1,9 @@
 #include <cassert>
 
-#include "../lib/icode_gen.h"
 #include "../lib/tcode_gen.h"
 
 extern quad *quads;
 extern unsigned total;
-
-avm_memcell nil_memcell(nil_m);
 
 void make_operand (expr *e, vmarg *arg) {
 
@@ -61,34 +58,6 @@ void make_operand (expr *e, vmarg *arg) {
         }
     
         default: assert(0);
-    }
-}
-
-void avm_table::avm_tableincrefcounter() {
-    refCounter++;
-}
-
-void avm_table::avm_decrefcounter() {
-    assert(refCounter > 0);
-    if (!--refCounter) {
-        delete this;
-    }
-}
-
-const avm_memcell avm_table::avm_tablegetelem(const avm_memcell& key) const {
-    auto pair = indexed.find(key);
-    if (pair != indexed.end()) {
-        return pair->second;
-    }
-    return nil_memcell;
-}
-
-void avm_table::avm_tablesetelem(const avm_memcell& key, const avm_memcell& value) {
-    if (value.type == nil_m) {
-        indexed.erase(key);
-    }
-    else {
-        indexed[key] = value;
     }
 }
 
