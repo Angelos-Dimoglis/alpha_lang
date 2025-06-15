19a0415ae6710b894cf0377d3f32baeba2ef42ad
Angelos-Dimoglis <angelos.titos@gmail.com>
2025-06-15 11:08:14 +0300

    add fiexed tests

--- testing/generate_bytecode.sh
+++ testing/generate_bytecode.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-for test_file in testing/tests/phase3/*.asc; do
+for test_file in testing/tests/phase4-5/*.asc; do
     filename=$(basename "$test_file" .asc)
     output_file="testing/byte_code/${filename}.abc"
     
-    echo "./alpha_compiler.out -i \"$test_file\" -b \"$output_file\""
+    echo "$test_file"
     ./alpha_compiler.out -i "$test_file" -b "$output_file" 1>/dev/null
 done
-
